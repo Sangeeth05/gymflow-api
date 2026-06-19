35c74e30d32a92090d979e6125ae163f7714c842
Sangeeth05 <sangeeth.ss2016@gmail.com>
2026-06-19 23:03:25 +0530

    changes to Enable Swagger on Render
    Enable Swagger on Render

--- Program.cs
+++ Program.cs
@@ -96,11 +96,11 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // ── Middleware pipeline ───────────────────────────────────────────────────────
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GymFlow API v1"));
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GymFlow API v1"));
+//}
 
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors("AllowFrontend");
